9a4208290e93f04a9ff2a179d7ba0ac9ab0867e8
satriajakaprayoga0707 <satriajakaprayoga0707@gmail.com>
2025-03-05 13:22:47 +0700

    feat(login): add route login

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', [Authentication::class, 'register']);
+Route::post('/login', [Authentication::class, 'login']);
